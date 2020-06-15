74e9bd458695a07ebef7c26595a2f816964678be
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 22:10:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do 
-    @product = params[:num1  :num2]
+    @product = params[:num1.to_i * :num2.to_i]
     "#{@product}"
   end
 end
